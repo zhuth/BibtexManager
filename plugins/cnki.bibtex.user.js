ac76172f650c4bb65306bc7c91018525551269bc
ZTH <zthpublic@gmail.com>
2013-10-31 17:30:35 +0800

    cnki update

--- plugins/cnki.bibtex.user.js
+++ plugins/cnki.bibtex.user.js
@@ -1,22 +1,22 @@
-// ==UserScript==
+﻿// ==UserScript==
 // @name           cnki bibtex generator
 // @description    cnki bibtex generator
 // @namespace      http://tianhua.me/
 // @auth           zhuth
 // @version        0.1
 // @license        Public Domain
-// @include		   http://*.cnki.*/detail.aspx*
+// @include		   http://*.cnki.*/*.aspx*
 // ==/UserScript==
 
 var x=document.getElementsByClassName("picShow")[0].getElementsByTagName("a");
 var journal = x[1].innerText;
-var year = parseInt(x[4].innerText);
-var number = parseInt(x[4].innerText.substring(x[4].innerText.indexOf('年')+1));
+var year = parseInt(x[x.length - 2].innerText);
+var number = parseInt(x[x.length - 2].innerText.substring(x[4].innerText.indexOf('年')+1));
 var title = document.getElementsByTagName('h1')[0].innerText;
 var authors = document.getElementsByClassName('author')[0].innerText.split('【')[1].substring(3).trim().split('；');
 while (authors[authors.length - 1] == '') authors.pop();
 authors = authors.join(', ');
-var bibtex = "@article {article" + title + ",\r\n" + 
+var bibtex = "@article {" + authors.substring(0, 3) + year + title + ",\r\n" + 
 			"\tauthor = {" + authors + "},\r\n" +
 			"\ttitle = {" + title + "},\r\n" +
 			"\tjournal = {" + journal + "},\r\n" +
